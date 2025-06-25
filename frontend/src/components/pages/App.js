4cb7c6df1aec6ea42a176b111689783941c64815
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 14:16:19 -0700

    Update App.js

--- frontend/src/components/pages/App.js
+++ frontend/src/components/pages/App.js
@@ -1,1 +1,20 @@
-femo
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import CustomerList from "./pages/CustomerList";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Customers</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<CustomerList />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

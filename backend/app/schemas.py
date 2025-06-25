eb4ddff158154380a84eeadd96948e8abe5d7862
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 14:30:02 -0700

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
-class UserBase(BaseModel):
-    email: EmailStr
-
-class UserCreate(UserBase):
-    password: str
-
-class User(UserBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        orm_mode = True
-
 class CustomerBase(BaseModel):
     name: str
     email: EmailStr

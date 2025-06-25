a9ba2189049d48cc8bee8244d77146fd2476016c
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 14:34:30 -0700

    Update api.js

--- frontend/src/components/pages/api.js
+++ frontend/src/components/pages/api.js
@@ -4,4 +4,11 @@ const api = axios.create({
   baseURL: "http://localhost:8000",
 });
 
+let token = null;
+
+export const setToken = (newToken) => {
+  token = newToken;
+  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
 export default api;

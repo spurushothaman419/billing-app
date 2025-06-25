c237dbb8b98b058a5c9118588c627dd5f866a7fa
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 14:29:34 -0700

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,14 +1,11 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from fastapi_users.db import SQLAlchemyBaseUserTable
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+class User(SQLAlchemyBaseUserTable, Base):
+    pass
 
 class Customer(Base):
     __tablename__ = "customers"

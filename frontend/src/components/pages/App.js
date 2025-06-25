454e18743532993c0a724b8778aea7acce51ad9f
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 14:35:47 -0700

    Update App.js

--- frontend/src/components/pages/App.js
+++ frontend/src/components/pages/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CustomerList from "./pages/CustomerList";
 import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
         <Link to="/">Customers</Link> | <Link to="/login">Login</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<CustomerList />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <CustomerList />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>

0a88ae5a770454b860b37bf66ae79b55e490610a
spurushothaman419 <79559764+spurushothaman419@users.noreply.github.com>
2025-06-25 18:13:31 -0700

    Update auth.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,11 +1,15 @@
+# app/auth.py
+
 from fastapi_users.authentication import AuthenticationBackend
 from fastapi_users.authentication.strategy.jwt import JWTStrategy
 from fastapi_users.authentication.transport.bearer import BearerTransport
 
+SECRET = "SUPER_SECRET_JWT"  # Use an env var in production
+
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret="CHANGE_THIS_SECRET", lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
